13618087b418efbdfb16249a97378f53f5882c52
Gyeong-min <gm.lee@kaist.ac.kr>
2017-05-23 22:06:51 +0900

    LBP_pixel
    LBP based on pixel(not block)

--- TestForOCV/TestForOCV/LBP.cpp
+++ TestForOCV/TestForOCV/LBP.cpp
@@ -1,6 +1,43 @@
 #include "LBP.h"
+#include <iostream>
+#include  <math.h>                   
+#define PI 3.141592654
 
-Mat LBPdescriptor(Mat img,int p1, int p2)
+Mat LBPdescriptor(Mat img, int p1, int p2)
 {
-	return Mat();
+	Mat im_gray;
+	Mat im_lbp(img.rows, img.cols, CV_8UC1);	//To show lbp image
+	Mat hist_lbp = Mat::zeros(256, 1, CV_32SC1);
+
+	//image to gray
+	if (img.channels() == 3)
+		cvtColor(img, im_gray, CV_RGB2GRAY);
+	else
+		im_gray = img;
+
+	unsigned center = 0;
+	unsigned center_lbp = 0;
+
+	for (int row = 1; row < im_gray.rows - 1; row++){
+		for (int col = 1; col < im_gray.cols - 1; col++){
+			center = im_gray.at<uchar>(row, col);
+			center_lbp = 0;
+
+			for (int i = 0; i < 8; i++){
+				if (center <= im_gray.at<uchar>(row - round(cos((1 - i) * PI / 4)), col - round(sin((1 - i) * PI / 4))))
+					//lbp cordinates
+					//std::cout << -round(cos((1 - i) * PI / 4)) << ", " << -round(sin((1 - i) * PI / 4)) << std::endl;
+					center_lbp += pow(2, i);
+			}
+
+			im_lbp.at<uchar>(row, col) = center_lbp;
+			hist_lbp.at<int>(center_lbp, 0)++;
+			//std::cout << hist_lbp.at<int>(center_lbp, 0) << std::endl;
+		}
+	}
+	//LBP image show
+	//imshow("lbp image",im_lbp);
+	//waitKey(0);
+	//std::cout << hist_lbp << std::endl;
+	return hist_lbp;
 }
