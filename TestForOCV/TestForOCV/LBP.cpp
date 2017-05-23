437ede341958209bcbbbf4277567026a3855fa5d
opo6954 <leehyeopwoo@kaist.ac.kr>
2017-05-23 22:26:57 +0900

    L2 distance debugging
    L2 distance 와 LBP vector간의 index 문제 해결

--- TestForOCV/TestForOCV/LBP.cpp
+++ TestForOCV/TestForOCV/LBP.cpp
@@ -3,6 +3,11 @@
 #include  <math.h>                   
 #define PI 3.141592654
 
+double round(double value)
+{
+	return value < 0.0 ? ceil(value - 0.5) : floor(value + 0.5);
+}
+
 Mat LBPdescriptor(Mat img, int p1, int p2)
 {
 	Mat im_gray;
@@ -24,6 +29,7 @@ Mat LBPdescriptor(Mat img, int p1, int p2)
 			center_lbp = 0;
 
 			for (int i = 0; i < 8; i++){
+				
 				if (center <= im_gray.at<uchar>(row - round(cos((1 - i) * PI / 4)), col - round(sin((1 - i) * PI / 4))))
 					//lbp cordinates
 					//std::cout << -round(cos((1 - i) * PI / 4)) << ", " << -round(sin((1 - i) * PI / 4)) << std::endl;
