8e8135a80a4ebc21a59dfb8783bba7ae78ef3f71
choijh0324 <jeehye0324@hotmail.com>
2017-07-05 21:35:36 +0900

    header refinement
    comment

--- TestForOCV/TestForOCV/BoVW.h
+++ TestForOCV/TestForOCV/BoVW.h
@@ -3,33 +3,44 @@
 #include "resource.h"
 #include <list>
 
+/**
+	@class BoVW
+	@date  2017/07/05
+	@brief class which makes and matches SIFT features using bag of visual words
+*/
 class BoVW
 {
 public:
-	BoVW(int words, int numImages);
-	void BoVW_maker();
-	void BoVW_Init();
-	std::vector<std::string> BoVW_matcher(cv::Mat img,int number);
+	BoVW(int words, int numImages);	//constructor
+	void BoVW_maker(); //bag making function
+	void BoVW_Init(); //bag initialization - making inverted tf-idf
+	std::vector<std::string> BoVW_matcher(cv::Mat img,int number); // matching function given an image
+
 private:
 	typedef struct scoring
 	{
 		std::string index;
 		float score;
-	}scoring;
+	}scoring; //use for scoring
 	typedef struct invertedList
 	{
 		int image;
 		float TF;
-	}invertedList;
-	cv::Mat dictionary;
-	cv::flann::Index kdTree;
+	}invertedList; // use for TF version
+	cv::Mat dictionary; //bag of words
+	cv::flann::Index kdTree; // kd tree
+
+	//items for TF-IDF
 	std::vector <unsigned char> F;
 	std::vector <float> IDF;
 	std::vector <scoring> score;
 	std::vector <std::list<invertedList>>Invert;
+	
+	//parameters
 	int numDocument;
 	int NUMVOCAB;
 	
+	//private functions which compute TF-IDF
 	void InsertTF(int t, int name, int numFeatures);
 	void InsertF(int t);
 	void ClearF();
